743dfbc71facb47bead9dfe7b9e89a43c5e40156
keb-web <kebinhleh@gmail.com>
2024-06-27 15:03:26 -0500

    Removed plugins flash, screenkey, trouble, hardtime/tryhard

--- lua/custom/plugins/tryhard.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'm4xshen/hardtime.nvim',
-  -- TODO: maybe change later, add toggle keybindings
-  enabled = false,
-  dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
-  opts = {},
-}
