53981c50bc7330e34e071e8e69992337c92907f9
keb-web <kebinhleh@gmail.com>
2024-07-29 19:12:48 +0000

    whichkey reformatting

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -3,31 +3,50 @@
 return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup {
-      triggers_blacklist = {
-        v = { '1', '2', '3', '4', '5' },
-        n = { '1', '2', '3', '4', '5' },
-        i = {},
-      },
+  config = function()
+    require('which-key').setup()
+    require('which-key').add {
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>g', group = '[G]it', mode = { 'n', 'v' } },
+      { '<leader>m', group = '[M]arkdown' },
     }
-
-    require('which-key').register({
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-      ['<leader>m'] = { name = '[M]arkdown', _ = 'which_key_ignore' },
-    }, {
-      ignore_missing = true,
-    })
-
-    -- visual mode
-    -- require('which-key').register({
-    --   '<leader>gh' = { '[G]it [H]unk' },
-    -- }, { mode = 'v' })
   end,
 }
+
+-- - WARNING In mode `n`, <yS> overlaps with <ySS>:
+--   - <yS>: Add a surrounding pair around a motion, on new lines (normal mode)
+--   - <ySS>: Add a surrounding pair around the current line, on new lines (normal mode)
+-- - WARNING In mode `n`, <ys> overlaps with <yss>:
+--   - <ys>: Add a surrounding pair around a motion (normal mode)
+--   - <yss>: Add a surrounding pair around the current line (normal mode)
+-- - WARNING In mode `n`, <gb> overlaps with <gbc>:
+--   - <gb>: Comment toggle blockwise
+--   - <gbc>: Comment toggle current block
+-- - WARNING In mode `n`, <gc> overlaps with <gco>, <gcO>, <gcc>, <gcA>:
+--   - <gc>: Comment toggle linewise
+--   - <gco>: Comment insert below
+--   - <gcO>: Comment insert above
+--   - <gcc>: Comment toggle current line
+--   - <gcA>: Comment insert end of line
+-- - WARNING In mode `n`, <dm> overlaps with <dm<Space>>, <dm->, <dm0>, <dm1>, <dm8>, <dm7>, <dm6>, <dm5>, <dm3>, <dm2>, <dm=>, <dm9>, <dm4>:
+-- - WARNING In mode `x`, <a> overlaps with <al>, <a%>, <an>:
+--   - <a>: Around textobject
+--   - <al>: Around last textobject
+--   - <an>: Around next textobject
+-- - WARNING In mode `x`, <i> overlaps with <il>, <in>:
+--   - <i>: Inside textobject
+--   - <il>: Inside last textobject
+--   - <in>: Inside next textobject
+-- - WARNING In mode `o`, <i> overlaps with <il>, <in>:
+--   - <i>: Inside textobject
+--   - <il>: Inside last textobject
+--   - <in>: Inside next textobject
+-- - WARNING In mode `o`, <a> overlaps with <al>, <an>:
+--   - <a>: Around textobject
+--   - <al>: Around last textobject
+--   - <an>: Around next textobject
